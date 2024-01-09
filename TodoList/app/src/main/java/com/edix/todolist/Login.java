b47939c6565c94f3602b357c6e6a73444687ca34
sergiopalmero <sergiopalmero87@gmail.com>
2024-01-09 19:39:48 +0100

    Menu, botones en escucha y pasar de pantalla

--- TodoList/app/src/main/java/com/edix/todolist/Login.java
+++ TodoList/app/src/main/java/com/edix/todolist/Login.java
@@ -2,13 +2,47 @@ package com.edix.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    Button loginButton;
+    TextView signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Escondemos la barra de arriba en la activity de login
+        getSupportActionBar().hide();
+
+        //inicializamos la variable del loginButton y lo ponemos a la escucha
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            //Cuando damos al boton pasamos a la activity main. Creamos un nuevo intent
+            @Override
+            public void onClick(View v) {
+                //TODO: Login en FiraBase
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //Inicializamos la variable del signUpButton y la ponemos a la escucha
+        signUpButton = findViewById(R.id.signUp);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Crear usuario en FiraBase
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- TodoList/app/src/main/java/com/edix/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/edix/todolist/MainActivity.java
@@ -1,8 +1,11 @@
 package com.edix.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Mostramos el menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Ponemos en escucha a los botones
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.addTask){
+            return true;
+        }else if(item.getItemId() == R.id.logOut){
+            return true;
+        }else{
+            return super.onOptionsItemSelected(item);
+        }
+
     }
 }
